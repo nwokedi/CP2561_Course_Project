cfe7ae148a1971af778b4fdce8d78e2112a7afed
nwokedi <65553824+nwokedi@users.noreply.github.com>
2022-08-06 16:29:56 -0230

    Initial commit

--- Hangman.java
+++ Hangman.java
@@ -7,6 +7,7 @@ public class Hangman {
     public static void main(String[] args) {
         Random random = new Random();
         String word;
+        int wrongGuessCounter = 0;
         Scanner sc = new Scanner("\"C:\\Users\\Kedi\\Desktop\\contacts.txt");
         List<String> words = new ArrayList<>();
 
@@ -14,6 +15,9 @@ public class Hangman {
             words.add(sc.nextLine());
         }
         word = words.get(random.nextInt(words.size()));
+        System.out.println("Please enter word: ");
+
+        List<Character> Guesses = new ArrayList<>();
 
     }
 }
