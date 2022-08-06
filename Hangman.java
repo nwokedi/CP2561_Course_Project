4fb42862ab8eb0b5b172591c74e3f7fc74b4bc75
nwokedi <65553824+nwokedi@users.noreply.github.com>
2022-08-06 16:09:02 -0230

    Create Hangman.java

---
+++ Hangman.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        Random random = new Random();
+        String word;
+        Scanner sc = new Scanner("\"C:\\Users\\Kedi\\Desktop\\contacts.txt");
+        List<String> words = new ArrayList<>();
+
+        while (sc.hasNext()) {
+            words.add(sc.nextLine());
+        }
+        word = words.get(random.nextInt(words.size()));
+
+    }
+}
